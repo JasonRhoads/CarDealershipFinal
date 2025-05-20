7960baa8bfc7251d9cfbf119abcee986604b9747
Jason Rhoads <jasrhoads@gmail.com>
2025-05-20 14:18:05 -0700

    clean up CarColorsDB

--- CarDealershipFinal/DatabaseFiles/CarColorsDB.cs
+++ CarDealershipFinal/DatabaseFiles/CarColorsDB.cs
@@ -5,41 +5,89 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CarDealershipFinal.DatabaseFiles
 {
+    /// <summary>
+    /// Get Car colors from file
+    /// </summary>
     public class CarColorsDB
     {
-        private const string Path = @"..\..\DataFiles\CarColors.txt";
+        private const string Directory = @"..\..\DataFiles\";
+        private const string Path = Directory + "CarColors.txt";
 
         /// <summary>
         /// Gets the age range data from the data file and returns it as a dictionary
         /// ex. 
         /// </summary>
-        /// <param></param>
         /// <returns>If successful, returns a Dictionary with keys equal to the names of the ranges
         /// and values equal to the minimum and maximum values of the keys
         /// </returns>
         public static List<string> Get()
         {
-            StreamReader textIn = new StreamReader(new FileStream(Path, FileMode.Open, FileAccess.Read));
+            List<string> colors = new List<string>();
+            try
+            {
+                StreamReader carsIn = new StreamReader(new FileStream(Path, FileMode.Open, FileAccess.Read));
 
-            var s = textIn.ReadToEnd().Split('|').ToList();
-            textIn?.Close();
-            return s;
+                colors = carsIn.ReadToEnd().Split('|').ToList();
+                carsIn?.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show(Path + " not found.", "File Not Found");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show(Directory + " not found.", "Directory Not Found");
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "IOException");
+            }
+            return colors;
         }
 
+        /// <summary>
+        /// Saves color to file only if it is a new color
+        /// </summary>
+        /// <param name="color"></param>
         public static void Save(string color)
         {
-            //StreamReader textIn = new StreamReader(new FileStream(Path, FileMode.Open, FileAccess.Read));
+            try
+            {
+                //read the current list of colors
+                StreamReader colorIn = new StreamReader(new FileStream(Path, FileMode.Open, FileAccess.Read));
+                string result = colorIn.ReadToEnd();
+                colorIn?.Close();
 
-            string result = System.IO.File.ReadAllText(Path);
-            if (!result.Contains(color))
-                result += $"|{color}";
-            System.IO.File.WriteAllText(Path, result);
-            //textIn.Close();
+                if (!result.Contains(color))
+                    result += $"|{color}";
+
+                //write the new list of colors
+                StreamWriter colorOut = new StreamWriter(new FileStream(Path, FileMode.Create, FileAccess.Write));
+                colorOut.Write(result);
+                colorOut?.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show(Path + " not found.", "File Not Found");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show(Directory + " not found.", "Directory Not Found");
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "IOException");
+            }
         }
 
+        /// <summary>
+        /// Updates Car colors after a car has been deleted
+        /// </summary>
+        /// <param name="colors"></param>
         public static void Update(List<string> colors)
         {
             string result = "";
@@ -48,7 +96,25 @@ namespace CarDealershipFinal.DatabaseFiles
                 if (!result.Contains(color))
                     result += $"|{color}";
             }
-            System.IO.File.WriteAllText(Path, result.Substring(1));
+            try
+            {
+                //write the new list of colors
+                StreamWriter colorOut = new StreamWriter(new FileStream(Path, FileMode.Create, FileAccess.Write));
+                colorOut.Write(result.Substring(1));
+                colorOut?.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show(Path + " not found.", "File Not Found");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show(Directory + " not found.", "Directory Not Found");
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message, "IOException");
+            }
         }
     }
 }
